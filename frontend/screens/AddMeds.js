b5893b94e5cf44b67a4c1d8649f1b96fb2ac00c7
t-iffany <tiffanyxleong@gmail.com>
2023-05-21 02:08:46 -0700

    fix timer

--- frontend/screens/AddMeds.js
+++ frontend/screens/AddMeds.js
@@ -1,83 +1,50 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const MedicationReminderForm = () => {
-  const [pillName, setPillName] = useState('');
-  const [selectedDays, setSelectedDays] = useState([]);
+const AddMeds = () => {
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [showPicker, setShowPicker] = useState(false);
 
-  const handlePillNameChange = (value) => {
-    setPillName(value);
+  const handleTimeChange = (event, time) => {
+    setShowPicker(false);
+    if (time !== undefined) {
+      setSelectedTime(time);
+    }
   };
 
-  const handleDayToggle = (day) => {
-    const updatedDays = selectedDays.includes(day)
-      ? selectedDays.filter((selectedDay) => selectedDay !== day)
-      : [...selectedDays, day];
-
-    setSelectedDays(updatedDays);
+  const handleSetTime = () => {
+    setShowPicker(true);
   };
 
-  const handleSubmit = () => {
-    // Perform form submission logic here
-    console.log('Pill Name:', pillName);
-    console.log('Selected Days:', selectedDays);
-    // You can save the reminder data or perform any other actions
-
-    // Clear form fields
-    setPillName('');
-    setSelectedDays([]);
+  const handleSaveTime = () => {
+    // Handle saving the selectedTime to local storage or perform other actions
+    console.log(selectedTime);
+    // ...
   };
-
-  const isDaySelected = (day) => selectedDays.includes(day);
 
   return (
-    <View >
-      <TextInput
-        placeholder="Pill Name"
-        value={pillName}
-        onChangeText={handlePillNameChange}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 20 }}>
+      <Button title="Set Time" onPress={handleSetTime} />
 
-      <View style={styles.daySelectorContainer}>
-        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day) => (
-          <TouchableOpacity
-            key={day}
-            style={[styles.dayButton, isDaySelected(day) && styles.selectedDayButton]}
-            onPress={() => handleDayToggle(day)}
-          >
-            <Text style={[styles.dayButtonText, isDaySelected(day) && styles.selectedDayButtonText]}>{day}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+      {showPicker && (
+        <DateTimePicker
+          value={selectedTime || new Date()}
+          mode="time"
+          is24Hour={true}
+          display="spinner"
+          onChange={handleTimeChange}
+        />
+      )}
 
-      <Button title="Set Reminder" onPress={handleSubmit} />
+      {selectedTime && (
+        <View>
+          <Text>Selected Time: {selectedTime.toLocaleTimeString()}</Text>
+          <Button title="Save Time" onPress={handleSaveTime} />
+        </View>
+      )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  daySelectorContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 10,
-  },
-  dayButton: {
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    width: '14%',
-    alignItems: 'center',
-  },
-  selectedDayButton: {
-    backgroundColor: '#ccc',
-  },
-  dayButtonText: {
-    fontSize: 16,
-  },
-  selectedDayButtonText: {
-    fontWeight: 'bold',
-  },
-});
-
-export default MedicationReminderForm;
+export default AddMeds;
